cb891786f7c09f22afcfb0274f1661c2363abed2
Melyn Were <werema@cardiff.ac.uk>
2023-10-18 22:19:40 +0100

    length of array for shortcut syntax

--- src/Arrays.java
+++ src/Arrays.java
@@ -26,6 +26,9 @@ public class Arrays {
 
         //Shortcut syntax to create and initialize an array
         //int[] myArray = {100,200,300,400,500};
+        /**the length of the array will be determined by 
+        the number of values between the braces, separated by commas**/
+        
         
     }
 }
