910665607f896156aca1e2cb7e6dc02e70c96200
Melyn Were <werema@cardiff.ac.uk>
2023-10-18 22:01:33 +0100

    allocate memory for 5 integers in the array

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,6 +1,9 @@
 public class Arrays {
     public static void main(String[] args){
         //declare an array of integers
-        int[] myArray
+        int[] myArray;
+
+        //allocates memory for 5 integers
+        myArray = new int[5];
     }
 }
