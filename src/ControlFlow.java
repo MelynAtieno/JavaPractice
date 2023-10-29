10971c53870fceb83e28823f95b98fd979888345
Melyn Were <werema@cardiff.ac.uk>
2023-10-29 23:59:08 +0000

    add for loop control flow

---
+++ src/ControlFlow.java
@@ -0,0 +1,13 @@
+public class ControlFlow {
+    //For Loop
+    public static void main(String[] args){
+        int[] anArray = {10,20,30,40,50};
+
+        for(int i = 0; i < anArray.length; i++){
+            System.out.println(anArray[i]);
+        }
+
+        
+    }
+    
+}
