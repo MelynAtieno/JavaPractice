58c2f37077dafd2a38f4b22e59daf399d82ba2d5
Melyn Atieno Were <melynatieno@gmail.com>
2023-11-03 22:19:16 +0000

    add switch case statements

--- src/ControlFlow.java
+++ src/ControlFlow.java
@@ -29,6 +29,51 @@ public class ControlFlow {
             l++;
         } while(l < a2.length);
 
+        //switch statement
+        int month = 3;
+        String monthName;
+         switch (month) {
+            case 1: monthName = "January";
+            break;
+            case 2: monthName = "February";
+            break;
+            case 3: monthName = "March";
+            break;
+            case 4: monthName = "April";
+            break;
+            case 5: monthName = "May";
+            break;
+            case 6: monthName = "June";
+            break;
+            case 7: monthName = "July";
+            break;
+            case 8: monthName = "August";
+            break;
+            case 9: monthName = "September";
+            break;
+            case 10: monthName = "October";
+            break;
+            case 11: monthName = "November";
+            break;
+            case 12: monthName = "December";
+            break;
+            default: monthName= "Invalid Month";
+            
+         }
+         System.out.println(monthName);
+
+         //switch statement arrow version
+         int love = 2;
+         String fruit;
+         switch (love) {
+            case 1 -> fruit = "Apple";
+            case 2 -> fruit = "Pear";
+            case 3 -> fruit = "Banana";
+            default -> fruit = "Invalid fruit";
+            
+         }
+         System.out.println(fruit);
+
         
     }
         
