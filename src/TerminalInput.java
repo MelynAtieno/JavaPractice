20a41a0fdf56a59f3d1dfc3fb9dc70c429479cf4
Melyn Atieno Were <melynatieno@gmail.com>
2023-11-03 22:53:18 +0000

    add string scanner

--- src/TerminalInput.java
+++ src/TerminalInput.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class TerminalInput {
+    public static void main(String[] args){
+
+    Scanner stringScan = new Scanner(System.in); // Create a scanner object
+    System.out.println("What is your name?");
+
+    String name = stringScan.nextLine(); // Read user input
+    System.out.println("My name is:"+ " " + name + "."); // Output user input
+
+    stringScan.close();
+    
+    
+    }
     
 }
