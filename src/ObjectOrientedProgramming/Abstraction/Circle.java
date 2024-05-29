70271e9c7fe85e4f68a33ffb843d2dc9685ff460
Melyn Were <werema@cardiff.ac.uk>
2024-05-29 22:41:31 +0100

    add circle class

---
+++ src/ObjectOrientedProgramming/Abstraction/Circle.java
@@ -0,0 +1,13 @@
+package ObjectOrientedProgramming.Abstraction;
+
+public class Circle extends Shapes {
+
+    public void findArea(){
+        System.out.println("Area : " + (3.14 * radius * radius));
+    }
+
+    public static void main(String[] args) {
+        Circle circle1 = new Circle();
+        circle1.findArea();
+    }
+}
